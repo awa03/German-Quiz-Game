1fa30466c1797aa247520ee01136ece2538a7eab
Aiden <118379307+awa03@users.noreply.github.com>
2024-02-13 23:54:58 -0500

    Add Highscore
    Adds high score, and resolves window issues

--- Windows/Quiz.py
+++ Windows/Quiz.py
@@ -4,6 +4,7 @@ import Windows.QuizLogic.Generate_Word as Generate_Word
 
 current_window = None
 incorrect_window = None
+high_score = 0
 
 def Start():
     global current_window
@@ -24,6 +25,7 @@ def translate_to_english(german_word):
 
 def Submit(sender, data):
     global incorrect_window
+    global high_score
     answer = pygui.get_value("answer_input")
     generate_word = pygui.get_value("word_text")
     english_translation = translate_to_english(generate_word)
@@ -32,10 +34,12 @@ def Submit(sender, data):
     if english_translation is not None:
         with pygui.window(label="Incorrect", width=600, height=200, on_close=Menu_Exit):
             if answer.lower() == english_translation.lower():
+                high_score += 1
                 Exit()
                 Start()
             else:
                 pygui.add_text(f"Incorrect: Correct answer: {english_translation}", parent="Incorrect")
+                pygui.add_text(f"Your score: {high_score}", parent="Incorrect")
                 incorrect_window = "Incorrect"
  
         pygui.set_value("answer_input", "")  # Clear the input field
@@ -45,15 +49,22 @@ def Submit(sender, data):
 def Exit():
     global current_window
     global incorrect_window
-
+    global high_score
+    pygui.delete_item(current_window)
     try:
-        pygui.delete_item(incorrect_window)
-        pygui.delete_item(current_window)
+        current_window = "Exit_Screen_Game"
+        with pygui.window(label="Exit_Screen_Game", width=600, height=200, on_close=Menu_Exit):
+            pygui.add_text(f"Your score: {high_score}", parent="Exit_Screen_Game")
+        
     except:
         pass
 
+    pygui.delete_item(current_window)
     current_window = None
 
 def Menu_Exit():
+    global incorrect_window
     pygui.delete_item(incorrect_window)
+    incorrect_window = None
         
+
